5f2c34c04b1492ad868e45e9c35f70dd1007a57e
mac <mac@macs-MacBook-Pro.local>
2018-07-03 19:07:54 -0600

    added more pics, function, css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log("linked up")
-
 //Survive in a death mansion.  Certain areas may be detrimental and cause health to 
 //reduce just by being in the room.  Some monsters automatically kill the player. (Monsters could be 
 //randomly generated based on location/floor).  Moving into a room instantiates a monster.
@@ -35,48 +33,90 @@ console.log("linked up")
 //(Monster name) belches and lights it on fire, burning your eyes and lungs.  You are damaged by the sheer 
 //awesomness!
 //
-//River monster attacks.  You die of dysentary.
+//A river monster attacks.  You die of dysentary.
+//
+//You kick (monster) in the gnards.  Against all known monster lore, it seems to work!
+//
+//The monster moves towards you with slavering jaws ready to chew your face off and you are comforted in the 
+//the knowledge that your imminent death is better than the Orwellian horror that awaits you outside, should you 
+//have managed to successfully escape.
+//
+//You curl up into a ball and let entropy take you.
 //
 //
 $(document).ready(function() {
     console.log( "ready!" );
 
+//---------------------Event Fricken' Do'ers!---------------------------
+$("#start-game-button").on("click", () => {
+	$(".btn").replaceWith("<button type='button' id='fight' class='btn btn-danger btn-lg btn-block'>Fight!</button>");
+	$(".buttons").append("<button type='button' id='run' class='btn btn-light btn-lg btn-block'>Run Away!</button>");
+	$(".buttons").append("<button type='button' id='main-hall' class='btn btn-dark btn-lg btn-block'>Main Hall</button>");
+	foyer();
+	startTimer();
+	const playerOne = new playerChar(10, 2, 2);
 
-
-
-
+$("#fight").on("click", () => {
+	console.log("Fight! Fight!")
+	playerOne.attack();
+	zombieOne.attack();
+	console.log(playerOne);
+	console.log(zombieOne);
+});
+$("#run").on("click", () => {
+	console.log("Run away!!!")
+	foyer();
+});
+$("#main-hall").on("click", () => {
+	mainHallway();
+	});
+});
+//----------------------------------------------------------------------
+//--------------------Class\Method Creation-----------------------------
 class monster {
-	constructor(name, health, damage, type){
-		this.name = name;
+	constructor(health, damage, resistance){
 		this.health = health;
 		this.damage = damage;
-		this.type  = type;
+		this.resistance = resistance;
 	}
 	attack(){
-		health--;
+		if (this.resistance <= playerChar.damage) {
+			this.health -= 2
+		} else if (this.resistance > playerChar.damage) {
+			this.health--
+		}
 	}
-
-
 
 }
 
 class playerChar {
-	constructor(name, health, damage){
-		this.name = name;
+	constructor(health, damage, resistance){
 		this.health = health;
 		this.damage = damage;
+		this.resistance = resistance;
 	}
 	attack(){
-
+		if (this.resistance <= monster.damage) {
+			this.health -= 2
+		} else if (this.resistance > monster.damage) {
+			this.health--
+		}
 	}
 }
+
+const zombieOne = new monster(6, 1, 1);
+//--------------------------------------------------------------------
+//-------------------------------Health Bar---------------------------
+
 
 $('#health-bar').css("width", (playerChar.health * 10)+ "%");
 
 
+//--------------------------------------------------------------------
+//---------------------------Countdown Timer------------------------------
 document.getElementById('survival-timer').innerHTML =
   20 + ":" + 00;
-startTimer();
+
 
 function startTimer() {
   var presentTime = document.getElementById('survival-timer').innerHTML;
@@ -84,7 +124,7 @@ function startTimer() {
   var m = timeArray[0];
   var s = checkSecond((timeArray[1] - 1));
   if(s==59){m=m-1}
-  //if(m<0){alert('timer completed')}
+  if(m===0){console.log('timer completed')}
   
   document.getElementById('survival-timer').innerHTML =
     m + ":" + s;
@@ -92,24 +132,25 @@ function startTimer() {
 }
 
 function checkSecond(sec) {
-  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; 
   if (sec < 0) {sec = "59"};
   return sec;
 }
-// const survivalTimerClock = () => {
-// 	let minutes = 1200000;
-// 	let seconds = 60;
+//---------------------------------------------------------------------------
+//----------------------------Room Functions----------------------------------------------------------------------------------
+const foyer = () => {
+	$(".game-screen").attr('src', 'images/door2.jpg');
+	$(".description").text("You've entered the foyer of a large and spooky house.  The door slams shut behind you and you can hear the clicks of some sort of mechanism ticking away.  You realize you only have to survive until the door opens...  Before you lay a long hallway with doors on both sides.")
+	//$(".sounds")[0].play();
+}
 
-// 	let seconds = Math.floor( (seconds/1000) % 60 );
-// 	let minutes = Math.floor( (minutes/1000/60) % 60 );
+const mainHallway = () => {
+	$(".game-screen").attr('src', 'images/mainfloorhall.jpg');
+	$(".description").text("You step into a large hall, at the end of which is a grand staircase.  Before you can see any more, a hideous zombie shuffles up to you asking for loose change!");
+	
+}
 
-// 		return {
-// 			"minutes" : minutes,
-// 			"seconds" : seconds
-// 	};
 
-// }
-// console.log(survivalTimerClock);
 
 
 
